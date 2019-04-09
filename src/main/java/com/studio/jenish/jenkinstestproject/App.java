bb2d692e03d7d2c11dbd807c65b0e13b3afbd804
jenishamarjan <jmaharja@villanova.edu>
2019-04-09 14:39:23 -0400

    Just another commit

--- src/main/java/com/studio/jenish/jenkinstestproject/App.java
+++ src/main/java/com/studio/jenish/jenkinstestproject/App.java
@@ -4,10 +4,8 @@ package com.studio.jenish.jenkinstestproject;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World! Peace!!");
     }
 }
